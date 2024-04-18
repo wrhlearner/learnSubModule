e4a104620ad3a5bef7c63cdd855f14bf2e597bf8
wrhlearner24 <18561226609@163.com>
2024-04-19 01:13:10 +0800

    update submodule from supermodule local

--- submodule.cpp
+++ submodule.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 
 void say_hello(std::string str){
-    std::cout << "Submodule update" << std::endl;
+    std::cout << "update submodule in supermodule" << std::endl;
     std::cout << str << std::endl;
 }
